39ba9e6afd7b52059fd5e30142e5e107efc54a84
junngo <myeongjun.ko@gmail.com>
2020-04-12 01:08:11 +0900

    created vertical movie on movie screen

--- screens/Movies/MoviesPresenter.js
+++ screens/Movies/MoviesPresenter.js
@@ -5,24 +5,24 @@ import Swiper from "react-native-web-swiper";
 import { ActivityIndicator, ScrollView, Dimensions } from "react-native";
 import Slide from "../../components/Movies/Slide";
 import Title from "../../components/Title";
+import Vertical from "../../components/Vertical";
 
 const { width: WIDTH, height: HEIGHT } = Dimensions.get("window");
 
 const SliderContainer = styled.View`
     width: ${WIDTH}px;
     height: ${HEIGHT / 4}px;
-    margin-bottom: 50px;
+    margin-bottom: 40px;
 `;
 
 const Container = styled.View``;
 
 
-export default ({ loading, nowPlaying }) => (
+export default ({ loading, nowPlaying, popular }) => (
   <ScrollView
-    style={{
-      backgroundColor: "black"
-    }}
+    style={{}}
     contentContainerStyle={{
+      backgroundColor: "black",
       flex: 1,
       justifyContent: loading ? "center" : "flex-start"
     }}
@@ -47,7 +47,22 @@ export default ({ loading, nowPlaying }) => (
           </Swiper>
         </SliderContainer>
         <Container>
-          <Title title={"Popoular Movies"} />
+          <Title title={"Popular Movies"} />
+          <ScrollView
+            style={{ marginTop: 20 }}
+            contentContainerStyle={{ paddingLeft: 30 }}
+            horizontal
+            showsHorizontalScrollIndicator={false}
+          >
+            {popular.map(movie => (
+              <Vertical
+                key={movie.id}
+                poster={movie.poster_path}
+                title={movie.title}
+                votes={movie.vote_average}
+              />
+            ))}
+          </ScrollView>
         </Container>
       </>
     )}
