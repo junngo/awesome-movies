5ae8846440db32f1afc36d00ad81698d2c030b7f
junngo <myeongjun.ko@gmail.com>
2020-04-11 17:34:42 +0900

    added stack and tabs Navigation

---
+++ navigation/Stack.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { createStackNavigator } from "@react-navigation/stack";
+import Tabs from "./Tabs";
+import Detail from "../screens/Detail";
+
+const Stack = createStackNavigator();
+
+export default () => (
+  <Stack.Navigator>
+    <Stack.Screen name="Tab" component={Tabs} />
+    <Stack.Screen name="Detail" component={Detail} />
+  </Stack.Navigator>
+);
